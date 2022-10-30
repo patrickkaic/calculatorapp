04950862e25095bf7f1910f497d649d9aa136696
Patrick Almeida <patrickkaic@gmail.com>
2022-10-30 17:12:06 -0300

    First commit

---
+++ lib/calculator.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class Calculator extends StatefulWidget {
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  String userInput = "";
+  String result = "0";
+
+  List<String> buttonsList = [
+    'C',
+    '(',
+    ')',
+    '/',
+    '7',
+    '8',
+    '9',
+    '*',
+    '4',
+    '5',
+    '6',
+    '+',
+    '1',
+    '2',
+    '3',
+    '-',
+    'AC',
+    '0',
+    '.',
+    '='
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: Column(
+        children: [
+          Flexible(child: resultWidget(), flex: 1),
+          Flexible(child: buttonsWidget(), flex: 2),
+        ],
+      )),
+    );
+  }
+
+  Widget resultWidget() {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.centerRight,
+          child: Text(
+            userInput,
+            style: const TextStyle(fontSize: 32),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.centerRight,
+          child: Text(
+            result,
+            style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buttonsWidget() {
+    return GridView.builder(
+      itemCount: buttonsList.length,
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+      itemBuilder: (BuildContext context, int index) {
+        return button(buttonsList[index]);
+      },
+    );
+  }
+
+  Widget button(String text) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: MaterialButton(
+        onPressed: () {
+          setState(() {
+            handleButtonPress(text);
+          });
+        },
+        color: getColor(text),
+        textColor: Colors.white,
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 25),
+        ),
+        shape: const CircleBorder(),
+      ),
+    );
+  }
+
+  handleButtonPress(String text) {
+    if (text == "AC") {
+      //resetar calculos
+      userInput = "";
+      result = "0";
+      return;
+    }
+
+    if (text == "C") {
+      //remover calculos
+      userInput = userInput.substring(0, userInput.length - 1);
+      return;
+    }
+
+    if (text == "=") {
+      //calcular resultados
+      result = calculate();
+      //Remover decimais if.0
+      if (result.endsWith(".0")) result = result.replaceAll(".0", "");
+      return;
+    }
+
+    userInput = userInput + text;
+  }
+
+  String calculate() {
+    try {
+      var exp = Parser().parse(userInput);
+      var evaluation = exp.evaluate(EvaluationType.REAL, ContextModel());
+      return evaluation.toString();
+    } catch (e) {
+      return "Error";
+    }
+  }
+
+  getColor(String text) {
+    if (text == "/" ||
+        text == "*" ||
+        text == "+" ||
+        text == "-" ||
+        text == "=") {
+      return Colors.orangeAccent;
+    }
+    if (text == "C" || text == "AC") {
+      return Colors.red;
+    }
+    if (text == "(" || text == ")") {
+      return Colors.blueGrey;
+    }
+    return Colors.lightBlue;
+  }
+}
